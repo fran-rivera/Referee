d03bc61abfa2d144a574c466b3f83310c76b9a81
FRoss <31376939+friverga@users.noreply.github.com>
2018-01-23 12:59:33 +0100

    Create project

---
+++ notBeFooled/src/main/java/com/hk/notBeFooled/NotBeFooledApplication.java
@@ -0,0 +1,12 @@
+package com.hk.notBeFooled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotBeFooledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotBeFooledApplication.class, args);
+	}
+}
